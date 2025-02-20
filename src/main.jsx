ad0b0134a3148b633c23a92b3b47ba0807989ae1
Farhan Yousafzai <coderwithferry@gmail.com>
2025-02-20 21:04:05 +0500

    inital commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,22 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+
+// Implementation of sentry/react
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+Sentry.init({
+  dsn: "https://d1de09eddd51af77801639fbbb1e9232@o4508852330561536.ingest.us.sentry.io/4508852332265472",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // 
+  tracesSampleRate: 1.0, 
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
